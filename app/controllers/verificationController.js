3c784b8d60dfd7b471a1626f8ba6a00cfe19081b
akmalariq <santoso54321@gmail.com>
2023-01-09 23:19:39 +0700

    add api/verify

--- app/controllers/verificationController.js
+++ app/controllers/verificationController.js
@@ -1,28 +1,52 @@
 const whatsappService = require("../services/whatsapp.service");
 
 const request = async (req, res, next) => {
-    try {
-        const {email, phone_number} = req.body;
-        const code = "1234";
+  try {
+    const { email, phone_number } = req.body;
+    const code = "1234";
 
-        if(!phone_number){
-            res.status(404).json({message:"Bad Request"})
-            return;
-        }
-
-        if(phone_number && !email )
-        await whatsappService.sendMessage(phone_number, code);
- 
-        res.status(200).json({
-            code: 200,
-            status: "success",
-            message: "Code sent successfully",
-          });
-    } catch (error) {
-        next(error);
+    if (!phone_number) {
+      res.status(404).json({ message: "Bad Request" });
+      return;
     }
-}
+
+    if (phone_number && !email)
+      await whatsappService.sendMessage(phone_number, code);
+
+    res.status(200).json({
+      code: 200,
+      status: "success",
+      message: "Code sent successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const verify = async (req, res, next) => {
+  try {
+    const { email, phone_number, code } = req.body;
+    // const code = "1234";
+    const valid_code = "1234";
+
+    console.log(email, code);
+
+    if ((!phone_number && !email) || !code || code != valid_code) {
+      res.status(404).json({ message: "Bad Request" });
+      return;
+    }
+
+    res.status(200).json({
+      code: 200,
+      status: "success",
+      message: "Code is correct",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 module.exports = {
-    request
-}
+  request,
+  verify,
+};
--- app/router/api/index.js
+++ app/router/api/index.js
@@ -1,18 +1,16 @@
-const verificationController  = require("../../controllers/verificationController");
+const verificationController = require("../../controllers/verificationController");
 
 const express = require("express");
 
 const router = express.Router();
 
-<<<<<<< HEAD
 router.get("/", (req, res) => {
   res.json({
     message: "Hello to the API",
   });
 });
-=======
 
 router.post("/verify/request", verificationController.request);
->>>>>>> 9c3acf18e46a838ebe6e96a0cab07db1fcbbf4fd
+router.post("/verify", verificationController.verify);
 
 module.exports = router;
