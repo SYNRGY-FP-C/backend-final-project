265b46ff2ba174ae05443fcc498bb86740ea3b2c
hasan-dev <hasan01nov@gmail.com>
2023-01-10 21:57:24 +0700

    add email

--- app/controllers/verificationController.js
+++ app/controllers/verificationController.js
@@ -1,17 +1,20 @@
 const whatsappService = require("../services/whatsapp.service");
+const emailService = require("../services/email.service");
 
 const request = async (req, res, next) => {
   try {
     const { email, phone_number } = req.body;
     const code = "1234";
 
-    if (!phone_number) {
+    if (!phone_number && !email) {
       res.status(404).json({ message: "Bad Request" });
       return;
     }
 
     if (phone_number && !email)
       await whatsappService.sendMessage(phone_number, code);
+
+    if (!phone_number && email) await emailService.sendEmail(email, code);
 
     res.status(200).json({
       code: 200,
